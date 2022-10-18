aade99d1f7f236bc05b654ba78adee131e0aa6f8
bogdanklymovskyy <bogdanklymovskyy@gmail.com>
2022-10-18 13:56:04 +0200

    update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
 
-console.log(galleryItems);
+class Image{
+    constructor({preview, original, description}={}){
+        this.source = original;
+        this.src = preview;
+        this.alt = description;
+    }
+}
+
+const array = [];
+
+createGalleryItemEl(galleryItems);
+
+function createGalleryItemEl (arr){
+for(const el of arr){
+const galleryImage = new Image(el);
+array.push(`
+<a class="gallery__item" href="${galleryImage.source}">
+  <img class="gallery__image" title ="${galleryImage.alt}"  src="${galleryImage.src}" alt="${galleryImage.alt}" />
+</a>`);
+}
+}
+
+galleryEl.insertAdjacentHTML('beforeend',`${array.join('')}`);
+
+const lightBoxGallery = new SimpleLightbox('.gallery a');
+lightBoxGallery.on('show.simplelightbox', function (){});
+console.log(lightBoxGallery.defaultOptions.captions);
+lightBoxGallery.defaultOptions.captionDelay = 250;
